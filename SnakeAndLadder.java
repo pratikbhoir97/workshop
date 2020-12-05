f2425ec4e24dca5a0e590de3f1731f769809e7f7
pratik bhoir <[bhoirpratik91@gmail.com]>
2020-12-05 14:39:51 +0530

    added switch cases for player next move

--- SnakeAndLadder.java
+++ SnakeAndLadder.java
@@ -5,10 +5,24 @@ public class SnakeAndLadder{
     static int playDice;
     static int playerMove;
     static Random random = new Random();
+    static int key;
+    static Scanner scanner = new Scanner(System.in);
     public static void main(String[] args) {
         playDice = throwDice();
         playerMove = play();
-
+        
+    }
+    
+    //This method will genarate random Number for DiceRoll
+    static int throwDice(){
+    playDice = random.nextInt(6)+1; 
+    System.out.println(playDice);
+    return playDice;
+    }
+    //This method will define next move for player
+    static int play(){
+        playerMove = random.nextInt(3)+1; 
+        System.out.println(playerMove);
         if(playerMove == 1){
             System.out.println("No Play");
         }else if(playerMove == 2){
@@ -17,22 +31,22 @@ public class SnakeAndLadder{
         }else{
             System.out.println("Snake");
             position -= playDice;
+            if(position<0)
+            position = 0;
         }
-        System.out.println(position);
+        System.out.println("User current position "+position);
+
+        System.out.println("enter 1 for next play any key for exit");
+        key = scanner.nextInt();
+        switch (key) {
+            case 1:
+                throwDice();
+                play();
+                break;
+            default:
+                break;
+        }
         
-    }
-
-    //This method will genarate random Number for DiceRoll
-    static int throwDice(){
-    playDice = random.nextInt(6)+1; 
-    System.out.println(playDice);
-    return playDice;
-    }
-
-    static int play(){
-        playerMove = random.nextInt(3)+1; 
-        System.out.println(playerMove);
-        return playerMove;
     }
 
 }
